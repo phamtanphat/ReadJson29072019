125c87776172a6f7701d28063e47abab218b3096
phatdroid94 <phatdroid94@gmail.com>
2019-09-16 19:17:29 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/readjson29072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/readjson29072019/MainActivity.java
@@ -5,49 +5,64 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.Callable;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity implements Observer<String>{
+public class MainActivity extends AppCompatActivity{
 
     Disposable disposable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Observable<String> stringObservable = Observable.just("A","B","C");
+        Observable<String> stringObservable = Observable.defer(new Callable<ObservableSource<? extends String>>() {
+            @Override
+            public ObservableSource<? extends String> call() throws Exception {
+                return Observable.just();
+            }
+        });
 
         stringObservable
                 .observeOn(Schedulers.io())
                 .subscribeOn(AndroidSchedulers.mainThread())
                 .subscribe(this);
     }
+    private String docNoiDung_Tu_URL(String theUrl){
+        StringBuilder content = new StringBuilder();
+        try    {
+            // create a url object
+            URL url = new URL(theUrl);
 
-    @Override
-    public void onSubscribe(Disposable d) {
-        if (d != null){
-            disposable = d;
+            // create a urlconnection object
+            URLConnection urlConnection = url.openConnection();
+
+            // wrap the urlconnection in a bufferedreader
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String line;
+
+            // read from the urlconnection via the bufferedreader
+            while ((line = bufferedReader.readLine()) != null){
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
         }
-
+        catch(Exception e)    {
+            e.printStackTrace();
+        }
+        return content.toString();
     }
 
-    @Override
-    public void onNext(String s) {
-        Log.d("BBB",s);
-    }
-
-    @Override
-    public void onError(Throwable e) {
-
-    }
-
-    @Override
-    public void onComplete() {
-        disposable.dispose();
-    }
 }
 
 
