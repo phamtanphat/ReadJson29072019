e67ed41072e175167270fcb6d464827230983dc4
phatdroid94 <phatdroid94@gmail.com>
2019-09-16 19:08:55 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/readjson29072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/readjson29072019/MainActivity.java
@@ -3,6 +3,7 @@ package phamtanphat.ptp.khoaphamtraining.readjson29072019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
@@ -12,11 +13,11 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements Observer<String>{
 
+    Disposable disposable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Observable<String> stringObservable = Observable.just("A","B","C");
 
         stringObservable
@@ -27,12 +28,15 @@ public class MainActivity extends AppCompatActivity implements Observer<String>{
 
     @Override
     public void onSubscribe(Disposable d) {
+        if (d != null){
+            disposable = d;
+        }
 
     }
 
     @Override
     public void onNext(String s) {
-
+        Log.d("BBB",s);
     }
 
     @Override
@@ -42,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements Observer<String>{
 
     @Override
     public void onComplete() {
-
+        disposable.dispose();
     }
 }
 
